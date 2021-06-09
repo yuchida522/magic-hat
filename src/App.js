4462222b5dcb222d2392ad4ccc7fe0e629b51d1a
yuchida522 <yuchida522@gmail.com>
2021-06-09 16:50:26 -0700

    Error handling, add landing page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './app.css'
 
 function App() {
 
+  const [play, setPlay] = useState(false)
   const [question, setQuestion] = useState('')
   const [icebreakers, setIceBreakers] = useState(Questions)
   const [toggle, setToggle] = useState(false)
@@ -16,12 +17,15 @@ function App() {
   const [time, setTime] = useState(0)
   const [start, setStart] = useState(false)
   const [newQuestion, setNewQuestion] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [message, setMessage] = useState('')
+
 
   function randomNumber(){
     const randomNum = Math.floor(Math.random() * icebreakers.length)
     if (randomNumbers.length === icebreakers.length) {
-      setRandomNumbers([randomNum])
-      return randomNum
+      setRandomNumbers([])
+      setMessage("You've gone through all the questions! Click the question button to start over")
     } else if (randomNumbers.includes(randomNum)){
       return randomNumber()
     } else {
@@ -39,8 +43,16 @@ function App() {
     const timeInSeconds = (e.target.value) * 1000
     setTime(timeInSeconds)
   };
-  console.log(start, time)
-
+ console.log(time)
+  function startTimer(){
+    if (time === 0){
+      setErrorMessage("You must enter a time")
+      return
+    } else {
+      setStart(!start)
+      setErrorMessage('')
+    }
+  }
   useEffect(() => {
     let interval
     if (start) {
@@ -52,10 +64,17 @@ function App() {
   }, [start, time])
 
   function cancel(){
+    setStart(false)
     setToggle(false)
     setTime(0)
     setQuestion('')
-  }
+  };
+  
+  function close(){
+    setToggleQuestionInput(false)
+    setNewQuestion('')
+    setMessage('')
+  };
 
   function handleInput(e){
     const newQuestion = e.target.value
@@ -63,32 +82,49 @@ function App() {
   }
 
   function addQuestion(){
-    setIceBreakers([...icebreakers, newQuestion]);
+    if (!newQuestion){
+      setErrorMessage('You must enter a question')
+      return
+    } else {
+      setErrorMessage('Question added!')
+      setIceBreakers([...icebreakers, newQuestion]);
+    }
   }
 
   return (
     <div className="App">
+      {!play ? (
+        <div className="header">
+        <h1>Magic Hat</h1>
+        <h4>an icebreaker game</h4>
+        <Button variant="primary" onClick={()=> setPlay(true)}>Play</Button>
+        </div>
+      )
+      :
+      (<div>
       <div className="questions">
-      <p>{question}</p>
+      {question ? (<p>{question}</p>) : (<p className="done">{message}</p>)}
       </div>
       <div className="magic-hat">
       <img src={magicHat}/>
       </div>
       <div className="buttons">
       <Button variant="primary" onClick={() => randomQuestion()}>Give me a question</Button>
-      <Button variant="secondary" onClick={() => setToggle(true)}>auto generate questions</Button>
+      <Button variant="secondary" onClick={() => setToggle(true)}>Auto generate questions</Button>
       <Button variant="light" onClick={() => setToggleQuestionInput(true)}>Add my own questions</Button>
       </div>
       <div className="inputFields">
-
       {toggle ?
       (<div className="inputFields-toggled">
         <div className="inputFields-toggled-input">
         <Input variant="small-input" placeholder="in seconds" onChange={(e) => handleTime(e)}/>
         </div> <br/>
         <div className="inputFields-toggled-btns">
-        <Button variant="submit" onClick={() => setStart(!start)}>{!start ? ("start"): ("stop")}</Button>
+        <Button variant="submit" onClick={() => startTimer()}>{!start ? ("start"): ("stop")}</Button>
         {!start ? (<Button variant="cancel" onClick={() => cancel()}>cancel</Button>) : null}
+        </div>
+        <div className="message"> 
+        {errorMessage ? (<p className="error">{errorMessage}</p>) : null}
         </div>
        </div>) : null}
 
@@ -99,10 +135,15 @@ function App() {
       </div>
       <div className="inputFields-toggled-btns">
       <Button variant="submit" onClick={() => addQuestion()}>Add</Button>
-      <Button variant="cancel" onClick={() => setToggleQuestionInput(false)}>close</Button>
+      <Button variant="cancel" onClick={() => close()}>close</Button>
+      </div>
+      <div className="message"> 
+      {errorMessage ? (<p className="error">{errorMessage}</p>) : null}
       </div>
       </div>) : null}
       </div>
+      </div>)}
+      
     </div>
   );
 }
